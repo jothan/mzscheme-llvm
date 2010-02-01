1fe04b1b30992576e8230ed8df584c6eb686e7fe
Jonathan Bastien-Filiatrault <joe@x2a.org>
2010-01-31 21:33:22 -0500

    Add license header and brokeness warnings.

--- llvm_private.c
+++ llvm_private.c
@@ -1,3 +1,22 @@
+/*
+  MzScheme to LLVM 2.6 wrapper
+  Copyright (C) 2010  Jonathan Bastien-Filiatrault
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* This is broken, we need to define this according to llvm-config. */
 #define _DEBUG
 #define _GNU_SOURCE
 #define __STDC_LIMIT_MACROS
